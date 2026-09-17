e989f819e878ccd631d97073cf9488b79c9cd8b3
Lucas Hochmann Rosa <hrlucas.dev@gmail.com>
2026-09-17 07:56:55 -0300

    chore: initialize project configuration and build tooling

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'build/**'],
+  },
+  {
+    files: ['vite.config.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-undef': 'error',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
